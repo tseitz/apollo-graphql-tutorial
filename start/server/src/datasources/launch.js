36f25441cf13de3de0b9d226151690eb9598ad80
tseitz <tdseitz10@gmail.com>
2020-04-23 20:42:27 -0500

    Step 3 Complete

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -14,7 +14,7 @@ class LaunchAPI extends RESTDataSource {
       : [];
   }
 
-  async getLaunchesById({ launchId }) {
+  async getLaunchById({ launchId }) {
     const response = await this.get("launches", { flight_number: launchId });
 
     return this.launchReducer(response[0]);
--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -23,7 +23,7 @@ const typeDefs = gql`
 
   type Mission {
     name: String
-    missionPatch(size: PatchSize): String
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
@@ -31,8 +31,28 @@ const typeDefs = gql`
     LARGE
   }
 
-  type Query {
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
     launches: [Launch]!
+  }
+
+  type Query {
+    launches(
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
     me: User
   }
